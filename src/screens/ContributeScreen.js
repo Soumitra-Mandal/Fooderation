9fe9797f4be19cc50c983e70873e976887a7e9b9
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-07-02 14:04:22 +0530

    contribution page added

--- src/screens/ContributeScreen.js
+++ src/screens/ContributeScreen.js
@@ -1,5 +1,21 @@
+import '../screenstyles/ContributeStyle.css';
+import image from './food.jpg';
+
 function Contribute() {
-    return <h2>Contribute</h2>;
+    return (
+    <div id = 'Contri'>
+ <h2 id='contribute'>Contribute to our recipe database!</h2>
+    <form id='form'>
+<input type='text' id='name' name='name' placeholder='Name' autoComplete='false'/>
+<input type='email' id='email' name='email' placeholder='Email' autoComplete='false'/>
+<input type = 'text' id = 'dish' name = 'dish' placeholder = "Name of Dish" autoComplete='false'/>
+<label htmlFor = 'file'> Upload Recipe PDF</label>
+<input type = 'file' id = 'file' name='file'/>
+<input type = 'submit' id = 'submit' name = 'submit'/>
+  </form>
+  <img id='food1' src={image} alt="Icon"/>
+    </div>
+      );
   }
 
 export default Contribute;
